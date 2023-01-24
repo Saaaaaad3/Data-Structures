e7d6b3b662c1fba628a234cbd870f6c9f4576bd4
SaadMansuri3 <41145915+SaadMansuri3@users.noreply.github.com>
2023-01-24 23:52:42 +0530

    CircularLinkedList

--- Java/Linked Lists/CircularLinkedList.java
+++ Java/Linked Lists/CircularLinkedList.java
@@ -1,2 +1,22 @@
 public class CircularLinkedList {
+
+    private Node head;
+    private Node tail;
+    public class Node {
+        public int value;
+        public Node Next;
+
+        private Node() {}
+
+        private Node(int num) {
+            this.value = num;
+        }
+    }
+
+    public void add(){
+
+    }
+
+
+
 }
